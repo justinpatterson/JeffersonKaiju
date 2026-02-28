25afb052f1433dfba1ced0b1f0945595ab1a7f4e
Justin Patterson <justinpatterson@live.com>
2026-02-27 19:39:18 -0500

    UFO Behavior stubs built out

--- Assets/_KAIJU/Scripts/Gameplay/EnemyBehavior.cs
+++ Assets/_KAIJU/Scripts/Gameplay/EnemyBehavior.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    public enum BehaviorState { Idle, Roaming, Abduct, GoTo, Dodge }
+    public enum BehaviorState { Idle = 0, Roaming = 1, Abduct = 2, GoTo = 3, Dodge = 4, Dead = -1}
     public BehaviorState state;
     protected virtual BehaviorState PickNextBehavior() 
     {
         return BehaviorState.Idle;
     }
-    protected virtual void StartBehavior() { }
+    protected virtual void StartBehavior() 
+    {
+        
+    }
     protected virtual void UpdateBehavior() { }
     protected virtual void StopBehavior() { }
 
+    private void Start()
+    {
+        state = PickNextBehavior();
+        StartBehavior();
+    }
+    private void Update()
+    {
+        UpdateBehavior();
+    }
+    public virtual void DisableEnemy() 
+    {
+        StopBehavior();
+        state = BehaviorState.Dead;
+
+        StartBehavior();
+    }
 
 }
--- Assets/_KAIJU/Scripts/Gameplay/Spaceship_EB.cs
+++ Assets/_KAIJU/Scripts/Gameplay/Spaceship_EB.cs
@@ -2,5 +2,115 @@ using UnityEngine;
 
 public class Spaceship_EB : EnemyBehavior
 {
-    
+    public float stateTransitionTimer = 5f;
+    public Transform spaceShipPivot;
+    public Transform groundOffsetPivot;
+    public float baseGroundOffset = 1.5f;
+    [Range(0,5f)]
+    public float sinFactor = 0.25f;
+    [Range(0, 5f)]
+    public float sinSpeed = 0.5f;
+
+    public Rigidbody rb;
+    public UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabRef;
+
+    public Vector3 roamingDirection = Vector3.zero;
+    public float roamingSpeed = 1f;
+
+    protected override BehaviorState PickNextBehavior()
+    {
+        int randomBehavior = Random.Range(0, 2);
+        BehaviorState randomState = (BehaviorState) randomBehavior;
+        return randomState;
+    }
+    protected override void StartBehavior()
+    {
+        base.StartBehavior();
+        stateTransitionTimer = 5f;
+
+        switch (state)
+        {
+            case BehaviorState.Idle:
+                break;
+            case BehaviorState.Roaming:
+                roamingDirection = new Vector3
+                (
+                    Random.Range(-1f, 1f),
+                    0f,
+                    Random.Range(-1f,1f)
+                );
+
+                break;
+            case BehaviorState.Abduct:
+                break;
+            case BehaviorState.GoTo:
+                break;
+            case BehaviorState.Dodge:
+                break;
+            case BehaviorState.Dead:
+                break;
+        }
+
+    }
+    protected override void StopBehavior()
+    {
+        base.StopBehavior();
+    }
+    protected override void UpdateBehavior()
+    {
+        base.UpdateBehavior();
+        
+        if (state == BehaviorState.Dead)
+            return;
+
+        HoverSinOffset();
+
+        switch (state)
+        {
+            case BehaviorState.Idle:
+                break;
+            case BehaviorState.Roaming:
+                transform.position = transform.position + roamingDirection.normalized * roamingSpeed * Time.deltaTime;
+                break;
+            case BehaviorState.Abduct:
+                break;
+            case BehaviorState.GoTo:
+                break;
+            case BehaviorState.Dodge:
+                break;
+            case BehaviorState.Dead:
+                break;
+        }
+
+
+        StateTimerBehavior();
+    }
+
+    void HoverSinOffset() 
+    {
+        float currentTime = Time.time;
+        float currentSin = Mathf.Sin(currentTime * sinSpeed);
+        float currentYHeight = (currentSin * sinFactor) + baseGroundOffset;
+        groundOffsetPivot.localPosition = Vector3.up * currentYHeight;
+    }
+    void StateTimerBehavior() 
+    {
+        stateTransitionTimer -= Time.deltaTime;
+        if (stateTransitionTimer < 0)
+        {
+            StopBehavior();
+            state = PickNextBehavior();
+            StartBehavior();
+        }
+    }
+
+    public override void DisableEnemy()
+    {
+        if (state == BehaviorState.Dead)
+            return;
+
+        base.DisableEnemy();
+        rb.useGravity = true;
+        grabRef.forceGravityOnDetach = true;
+    }
 }
